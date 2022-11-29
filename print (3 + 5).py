6635a7f630fcde1ff6a7eac4b2171ff53bd1498f
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2022-11-29 10:26:51 +0530

    Add files via upload

---
+++ print (3 + 5).py
@@ -0,0 +1,17 @@
+print (3 + 5)
+print (3 -5)
+print (3**5)
+name = "subhash kumar yadav"
+print (name)
+print (name.upper())
+print (name.lower())
+first_name = "subhash"
+last_name = "kumar"
+print (first_name + " " +last_name)
+print ("python")
+print ("\tpython")
+print ("\npython")
+print ("python")
+print ("courses; \n\tBCA  \n\tBCOM \n\tBTEC  \n\tBSC")
+message = "This is the bca and it's intresting"
+print (message)
